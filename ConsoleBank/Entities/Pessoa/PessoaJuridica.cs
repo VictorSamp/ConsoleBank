4fc89628188583a2153b2ad3b5b2ba63d3f76504
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 08:58:46 -0300

    feat: cria classe pessoa juridica

---
+++ ConsoleBank/Entities/Pessoa/PessoaJuridica.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleBank.Entities.Pessoa
+{
+    class PessoaJuridica : Pessoa
+    {
+        private string Cnpj { get; set; }
+
+        public PessoaJuridica(string nome, string endereco, string telefone, string cnpj)
+            : base(nome, endereco, telefone)
+        {
+            Cnpj = cnpj;
+        }
+    }
+}
