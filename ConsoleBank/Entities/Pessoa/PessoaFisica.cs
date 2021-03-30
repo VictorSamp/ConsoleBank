136e97fe9040e79166d242e1e82912abc95302d8
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 08:58:39 -0300

    feat: cria classe pessoa fisica

---
+++ ConsoleBank/Entities/Pessoa/PessoaFisica.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleBank.Entities.Pessoa
+{
+    class PessoaFisica : Pessoa
+    {
+        private string Cpf { get; set; }
+
+        public PessoaFisica(string nome, string endereco, string telefone, string cpf)
+            : base(nome, endereco, telefone)
+        {
+            Cpf = cpf;
+        }
+    }
+}
