58bfdb04c1ddeaf0c7574ea3c9a419ed9ead892f
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 14:59:37 -0300

    feat: cria classe conta com metodo sacar e depositar

---
+++ ConsoleBank/Entities/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleBank.Entities
+{
+    public class Conta
+    {
+        private long NumeroConta { get; set; }
+        private string NomeCliente { get; set; }
+        private decimal Saldo { get; set; } = 0;
+
+        public Conta()
+        {
+        }
+
+        public Conta(string nomeCliente, decimal saldo)
+        {
+            NumeroConta = new Random().Next();
+            NomeCliente = nomeCliente;
+            Saldo = saldo;
+        }
+
+        public bool Sacar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            Saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+    }
+}
