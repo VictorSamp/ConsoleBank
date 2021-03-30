8a436df04aa81a5b2d26228b94fc237c1bec1f2c
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 08:57:43 -0300

    refactor: remove propriedade data criacao

--- ConsoleBank/Entities/Contas/ContaCorrente.cs
+++ ConsoleBank/Entities/Contas/ContaCorrente.cs
@@ -4,8 +4,8 @@ namespace ConsoleBank.Entities.Contas
 {
     class ContaCorrente : Conta
     {
-        public ContaCorrente(long numeroConta, string nomeCliente, decimal saldo, decimal credito, DateTime dataCriacao)
-            : base(numeroConta, nomeCliente, saldo, credito, dataCriacao)
+        public ContaCorrente(long numeroConta, string nomeCliente, decimal saldo, decimal credito)
+            : base(numeroConta, nomeCliente, saldo, credito)
         {
 
         }
--- ConsoleBank/Entities/Contas/ContaPoupanca.cs
+++ ConsoleBank/Entities/Contas/ContaPoupanca.cs
@@ -4,8 +4,8 @@ namespace ConsoleBank.Entities.Contas
 {
     class ContaPoupanca : Conta
     {
-        public ContaPoupanca(long numeroConta, string nomeCliente, decimal saldo, decimal credito, DateTime dataCriacao)
-            : base(numeroConta, nomeCliente, saldo, credito, dataCriacao)
+        public ContaPoupanca(long numeroConta, string nomeCliente, decimal saldo, decimal credito)
+            : base(numeroConta, nomeCliente, saldo, credito)
         {
 
         }
