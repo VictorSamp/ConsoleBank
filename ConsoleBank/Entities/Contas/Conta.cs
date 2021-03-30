7891358d9fd97dfb1ff57bbda8f6e0d081fec056
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 08:05:16 -0300

    refactor: corrige encapsulamento da propriedade credito

--- ConsoleBank/Entities/Contas/Conta.cs
+++ ConsoleBank/Entities/Contas/Conta.cs
@@ -7,15 +7,14 @@ namespace ConsoleBank.Entities.Contas
         private long _numeroConta { get; }
         private string _nomeCliente { get; set; }
         private decimal _saldo { get; set; }
-        private decimal _credito { get; set; }
+        protected decimal _credito { get; set; }
         private DateTime _dataCriacao { get; }
 
-        public Conta(long numeroConta, string nomeCliente, decimal saldo, decimal credito)
+        public Conta(long numeroConta, string nomeCliente, decimal saldo)
         {
             _numeroConta = new Random().Next(0, 12000);
             _nomeCliente = nomeCliente;
             _saldo = saldo;
-            _credito = credito;
             _dataCriacao = DateTime.Now;
         }
 
