30d97fa58c6812f41a2afd1a8cc39a82b6a639af
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 09:16:00 -0300

    feat: cria metodo ObterOpcaoUsuario

--- ConsoleBank/Program.cs
+++ ConsoleBank/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ConsoleBank
+﻿using ConsoleBank.Entities.Contas;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleBank
 {
     class Program
     {
+        List<Conta> listaContas = new List<Conta>();
+
         static void Main(string[] args)
         {
+            string opcaoUsuario = ObterOpcaoUsuario();
+        }
 
+        static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Bem Vindo ao ConsoleBank!");
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine();
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            string opcaoEscolhida = Console.ReadLine().ToUpper();
+            return opcaoEscolhida;
         }
     }
 }
