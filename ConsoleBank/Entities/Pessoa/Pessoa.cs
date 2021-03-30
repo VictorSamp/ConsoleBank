a2c3f9bc6f2561e4aca6a7adc59f7ead8214717b
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-30 08:58:28 -0300

    feat: cria classe pessoa

---
+++ ConsoleBank/Entities/Pessoa/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleBank.Entities.Pessoa
+{
+    public abstract class Pessoa
+    {
+        private string Nome { get; set; }
+        private string Endereco { get; set; }
+        private string Telefone { get; set; }
+
+        public Pessoa(string nome, string endereco, string telefone)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Telefone = telefone;
+        }
+    }
+}
